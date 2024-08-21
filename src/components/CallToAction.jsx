1819c1390a5ebe9e8201cf311b08f7c2805defad
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2024-08-21 00:09:50 -0300

    day 8
    finished the webpage part of the project, now need to start the animation process

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
+import starImage from "../assets/star.png";
+import springImage from "../assets/spring.png";
 
 const CallToAction = () => {
   return (
-	<div>CallToAction</div>
+	<section className='bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip'>
+    <div className="container">
+      <div className="section-heading relative">
+      <h2 className='section-title'>Sign up for Free today!</h2>
+      <p className='section-description mt-5'>Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+      <img src={starImage} alt="Star" width={340} className="absolute -left-[390px] -top-[140px]" />
+      <img src={springImage} alt="Star" width={340} className="absolute -right-[360px] -top-[2px]" />
+      </div>
+      <div className="flex gap-2 mt-10 justify-center">
+        <button className='btn btn-primary'>Get for Free</button>
+        <button className='btn btn-text gap-1'><span>Learn more</span><ArrowRightIcon className="h-5 w-5" /></button>
+      </div>
+    </div>
+  </section>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import logo from "../assets/logosaas.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaPinterestSquare } from "react-icons/fa";
+import { FaSquareYoutube } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-	<div>Footer</div>
+	<footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+    <div className="container">
+      <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:h-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD98,#C2F0B1,#2FD8FE)] before:absolute">
+      <img src={logo} alt="Sass logo" height={20} width={50} className="relative" />
+      </div>
+      <nav className="flex md:justify-evenly md:items-center max-sm:flex-col max-sm:gap-6 mt-6">
+        <a href="">About</a>
+        <a href="">Features</a>
+        <a href="">Customers</a>
+        <a href="">Pricing</a>
+        <a href="">Help</a>
+        <a href="">Carrers</a>
+      </nav>
+      <div className="flex justify-center gap-6 mt-6">
+        <FaSquareXTwitter className="w-8 h-8" />
+        <FaSquareInstagram className="w-8 h-8"/>
+        <FaLinkedin className="w-8 h-8" />
+        <FaPinterestSquare className="w-8 h-8" />
+        <FaSquareYoutube className="w-8 h-8 " />
+      </div>
+      <p className="mt-6">&copy; 2024 Coded by: Pedro H. Alexandre. design by: Frontend Tribe. All rights reserved.</p>
+    </div>
+  </footer>
   )
 }
 
